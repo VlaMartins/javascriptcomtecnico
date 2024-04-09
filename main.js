a246b8a862ddbe625396e86afd9708609079f840
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-09 19:50:05 -0300

    1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const contadores = document.querySelectorAll (".contador");
+const contadores = document.querySelectorAll ("#dias_restantes");
 
 const dataAtual = new Date(); // cria um objeto data que recupera / busca a data atual 
 let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
