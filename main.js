8b08f4a419a377a7cc80b3b8eb5ccb13d3de6f3a
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-09 19:30:42 -0300

    1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const dataAtual = new Date();// cria um objeto data que recupera / busca a data atual 
-let dataNiver prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
+const dataAtual = new Date(); // cria um objeto data que recupera / busca a data atual 
+let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
 dataNiver = new Date (dataNiver+"T23:59:59");
 
