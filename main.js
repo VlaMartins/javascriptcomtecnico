871f5969016b77d536204641ff8fff3a635509ba
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-09 18:40:06 -0300

    1

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ while (diasQueFaltam < 0) {
     diasQueFaltam = calculaDias();
 }
 
-document.querySelector("#dias_restantes").textContent = "Falta " + diasQueFaltam + " Dias";
+document.querySelector("#dias_restantes").textContent = "Faltam " + diasQueFaltam + " Dias";
 
 function calculaDias() {
     let dataObjetivo = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
