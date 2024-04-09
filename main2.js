758926264b92a84b5cf2db7b9564ad15a860bb73
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-09 19:55:19 -0300

    1

--- main2.js
+++ main2.js
@@ -1,9 +1,11 @@
-const contadores = document.querySelectorAll (".contador");
-const tempoAtual = new Date(); //retorna data atual do computador
-const tempoObjetivo1 = new Date("2024-04-19T00:00:00"); // formato de data padrao com hifem e um T para tempo
-let tempofinal = tempoObjetivo1 - tempoAtual; 
+const contadores = document.querySelectorAll ("#dias_restantes");
 
-let segundos = Math.floor (tempofinal / 1000);
+const dataAtual = new Date(); // cria um objeto data que recupera / busca a data atual 
+let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
+dataNiver = new Date (dataNiver+"T23:59:59"); //mudando o valor para valor de data e indica por si so que os proximos valores são tempo
+let diasQueFaltam = Math.floor((dataNiver - dataAtual)/86400000) //24horas x 60min x 60seg X 1000milissegundos = minutos x segundos x horas x mil   TRANSFORMANDO MILISSSEGUNDOS EM DIAS
+
+let segundos = Math.floor (diasQueFaltam / 1000);
 let minutos = Math.floor (segundos / 60); 
 let horas = Math.floor (minutos / 60);
 let dias = Math.floor (horas / 24); 
@@ -12,25 +14,4 @@ segundos %=  60; // sinal de porcentagem traz o resto da divisao
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
-
-
-
-dfkdsjfdkçlfjdlkfjsdlkfjsdlkfjsdlkfj
-
-let diasQueFaltam = calculaDias();//
-
-while (diasQueFaltam < 0) {
-    alert("A data informada é menor que a data atual, atualize a página e insira uma nova data");
-    diasQueFaltam = calculaDias();
-}
-
-document.querySelector("#dias_restantes").textContent = "Faltam " + diasQueFaltam + " Dias";
-
-function calculaDias() {
-    let dataObjetivo = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
-    dataObjetivo = new Date(dataObjetivo + "T23:59:59");
-    return Math.floor((dataObjetivo - dataAtual) / 86400000);
-}
-
+contadores[0].textContent = dias + " dias " + horas + " horas " ;
