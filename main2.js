9856eb0398e67013907c0a7482bf0d1fad96c49d
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-14 18:05:53 -0300

    1

--- main2.js
+++ main2.js
@@ -5,7 +5,7 @@ const contadores = document.querySelectorAll ("#dias_restantes");
 const dataAtual = new Date(); // cria um objeto data que recupera / busca a data atual 
 let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
 dataNiver = new Date (dataNiver+"T23:59:59"); //mudando o valor para valor de data e indica por si so que os proximos valores são tempo
-let diasQueFaltam = Math.floor((dataNiver - dataAtual)/86400000) //24horas x 60min x 60seg X 1000milissegundos = minutos x segundos x horas x mil   TRANSFORMANDO MILISSSEGUNDOS EM DIAS
+let diasQueFaltam  = dataNiver  - dataAtual; //24horas x 60min x 60seg X 1000milissegundos = minutos x segundos x horas x mil   TRANSFORMANDO MILISSSEGUNDOS EM DIAS
 
 let segundos = Math.floor (diasQueFaltam / 1000);
 let minutos = Math.floor (segundos / 60); 
