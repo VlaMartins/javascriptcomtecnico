650c5b89efc614abb38e5584ad17c68c0daa95e0
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-14 18:37:24 -0300

    555

--- main2.js
+++ main2.js
@@ -19,3 +19,6 @@ horas %= 24;
 contadores[0].textContent = dias + " dias  "+horas+"  horas  "+minutos+"  minutos " +segundos+" segundos ";
 
 document.querySelector("#horario").textContent = (dataAtual);
+
+
+document.querySelector("#horario").textContent  = document.write ("Agora é " + now.getDay() + ", " + now.getDate() + " de " + now.getMonth() + " de " + now.getFullYear() );
