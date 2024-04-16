51c559ce7ebba244010f8870f5251f835b19ebff
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-16 18:48:18 -0300

    55

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-/* CONFORME VOCÊ EXPLICOU NA MEET*/
-
 const dataAtual = new Date(); // cria um objeto data que recupera / busca a data atual 
 let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
 dataNiver = new Date (dataNiver+"T23:59:59"); //mudando o valor para valor de data e indica por si so que os proximos valores são tempo
