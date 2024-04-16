54c0585c3a581b247ddf6d446a36700dffd0b1d7
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-16 20:07:52 -0300

    44

--- main3.js
+++ main3.js
@@ -1,8 +1,8 @@
 //quantos dias eu ja vivi
 const dataAtual = new Date(); 
-let dataNiver = prompt("Data do seu próximo aniversário: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
+let dataNiver = prompt("Data que você nasceu: (formato ANO-MÊS-DIA. Exemplo: 2024-05-30"); //é uma funcao nativa do js e navegador pra passar msg
 var diferencaEmMilissegundos = dataAtual - dataNascimento;
-var diasVividos = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
+var diasVividos = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24 * 12));
 
 document.querySelector("#dias_vividos").textContent = diasVividos;
 
